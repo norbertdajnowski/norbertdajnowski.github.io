780d0b85d2d7c84aa926ad55c2a37fe827b7b841
norbert.dajnowski <norbert.dajnowski@yorksj.ac.uk>
2021-02-24 23:13:56 +0000

    setup

--- index.php
+++ index.php
@@ -13,12 +13,6 @@ function getIp() {
 	xhttp.open('GET', 'http://ip.jsontest.com/', true);
 	xhttp.send();
 
-if(this.readyState == 4 && this.status == 200) {
-
-	var output = JSON.parse(this.responseText);
-	document.getElementById(“ip”).innerHTML = output.ip;
-
-}
 
 </script>
 
