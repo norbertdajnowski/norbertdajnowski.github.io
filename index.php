dbecc572913a968b6b1af543addc5dc4d00d32e2
norbertdajnowski <44067252+norbertdajnowski@users.noreply.github.com>
2021-02-24 22:59:45 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script></script>
+</head>
+<body>
+<p id=”id”></p>
+<button type="button" onclick="getIp()">Click me to get your ip address.</button>
+</body>
+</html>
